740b44598cecf3785b5b251474c91af2705edf13
lvsong77 <392759128@qq.com>
2020-03-31 09:51:33 +0800

    complete cart bottom bar

--- src/views/cart/childComps/CartBottomBar.vue
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="bottom-bar">
-    <check-button/>
+    <div class="check-content">
+      <check-button class="check-button"/>
+      <span>全选</span>
+    </div>
+
+    <div class="price">
+      合计：{{totalPrice}}
+    </div>
+
+    <div class="calculate">
+      去计算({{checkedLength}})
+    </div>
   </div>
 </template>
 
 <script>
   import CheckButton from 'components/content/checkButton/CheckButton'
 
+  import {mapGetters} from 'vuex'
+
   export default {
     name: 'CartBottomBar',
     components: {
       CheckButton
+    },
+    computed: {
+      ...mapGetters(['cartList']),
+      totalPrice() {
+        return '¥' + this.cartList.filter(item => {
+          return item.checked
+        }).reduce((preValue, item) => {
+          return preValue + item.price * item.count
+        }, 0).toFixed(2)
+      },
+      checkedLength() {
+        return this.cartList.filter(item => item.checked).length
+      }
     }
   }
 </script>
@@ -18,7 +44,31 @@
 <style scoped>
   .bottom-bar {
     height: 40px;
-    background-color: red;
+    background-color: #eee;
     position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+  }
+
+  .check-content {
+    display: flex;
+    align-items: center;
+  }
+
+  .check-button {
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+  }
+
+  .calculate{
+    background-color: red;
+    color: #fff;
+    margin-right: 5px;
+    height: 100%;
+    line-height: 40px;
+    padding: 0 10px;
   }
 </style>
